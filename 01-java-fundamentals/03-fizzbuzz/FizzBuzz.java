19eb214e357b26469c72bd34469511b96e2856d4
Hermine <hermineavagyan@yahoo.com>
2021-12-13 23:14:22 -0800

    FizzBuzz assignment finished

---
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzz.java
@@ -0,0 +1,40 @@
+public class FizzBuzz {
+    public String fizzBuzz(int number) {
+        //if divisable by 3 and 5
+        if (number% 3 == 0 && number % 5 == 0){
+            return "Fizzbuzz";
+        }
+        //if divisable by 3
+        else if (number % 3 == 0){
+            return "Fizz";
+        }
+        //if divisable by 5
+        else if (number % 5 == 0){
+            return "Buzz";
+        }
+        else {
+            return String.valueOf(number);
+        }
+
+    }
+    // Create an overloaded method for fizzBuzz
+    public String fizzBuzz(
+            // Parameters
+            int number,
+            String multOf3Word,
+            String multOf5Word,
+            String multOf15Word
+    ) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return multOf15Word;
+        } else if (number % 3 == 0) {
+            return multOf3Word;
+        } else if (number % 5 == 0)
+            return multOf5Word;
+        else {
+            return String.valueOf(number);
+        }
+    }
+
+    }
+
